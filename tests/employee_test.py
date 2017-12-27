9cd0af3e0cc7b7ccfe9031ce9e0ce774f2cce9dc
Kaio Teixeira <kaio.teco@gmail.com>
2017-12-27 14:43:00 -0200

    test ok

--- tests/employee_test.py
+++ tests/employee_test.py
@@ -5,48 +5,57 @@ from rest_framework.test import APIClient, APIRequestFactory, APITestCase
 
 class EmployeeTest(APITestCase):
     __data = {}
+    __create_employee = {}
 
     @property
-    def data(self):
-        return self.__data
+    def data_to_save(self):
+        return self.__data_to_save
+
+    @property
+    def data_create_employee(self):
+        return self.__data_create_employee
+
+    @pytest.fixture
+    def create_employee(self):
+        from src.models import Employee
+
+        employee = Employee(
+            name="New Kaio",
+            email="kaio_teco@hotmail.com",
+            department="TI")
+        employee.save()
+
+        self.__data_create_employee = employee
 
     @pytest.fixture
     def setUp(self):
         self.client = APIClient()
-        self.factory = APIRequestFactory()
         self.client.login(username='admin', password='default123')
-        self.__data = {'name': 'Kaio', 'email': 'kaio.teco@gmail.com', 'department': 'WebDevelopment'}
+        self.__data_to_save = {'name': 'Kaio', 'email': 'kaio.teco@gmail.com', 'department': 'WebDevelopment'}
+        self.create_employee()
 
     @pytest.mark.django_db
     def test_create_employee(self):
         response = self.client.post('/api/employees/',
-                                    self.__data,
+                                    self.__data_to_save,
                                     format='json',
                                     follow=True)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    """
     @pytest.mark.django_db
     def test_update_employee(self):
-        from src.models import Employee
-        self.__data = {'name': 'Kaio Edição', 'email': 'kaio.teco@gmail.com', 'department': 'Business'}
-
-        response = self.client.put('/api/employees/'.format(Employee.objects.all().last().id) + '/',
-                                   self.__data,
-                                   format='json',
-                                   content_type='application/json')
+        response = self.client.put('/api/employees/' + format(self.__data_create_employee.id) + '/',
+                                   self.__data_to_save,
+                                   format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     @pytest.mark.django_db
     def test_get_employee(self):
-        from src.models import Employee
-
-        response = self.client.get('/api/employee/'.format(Employee.objects.all().last().id) + '/')
+        response = self.client.get('/api/employees/' + format(self.__data_create_employee.id) + '/')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    """
 
     @pytest.mark.django_db
     def test_get_employees(self):
@@ -56,18 +65,14 @@ class EmployeeTest(APITestCase):
 
     @pytest.mark.django_db
     def test_get_not_found(self):
-        response = self.client.get('/api/employees/46545456/')
+        response = self.client.get('/api/employees/13/')
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    """
     @pytest.mark.django_db
     def test_delete(self):
-        from src.models import Employee
-
-        response = self.client.delete('/api/employees/'.format(Employee.objects.all().last().id) + '/',
-                                      json.dumps({"id": Employee.objects.all().last().id}),
+        response = self.client.delete('/api/employees/' + format(self.__data_create_employee.id) + '/',
+                                      {"id": self.__data_create_employee.id},
                                       format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-    """
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT, status.HTTP_200_OK)
